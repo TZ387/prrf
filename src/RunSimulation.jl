e335562e80280ff59a5a80b84bf30eb26facc049
Tilen Zel <tilen.zel@gmail.com>
2026-05-26 12:20:40 +0200

    Remove unused code in RunSimulation.jl

--- src/RunSimulation.jl
+++ src/RunSimulation.jl
@@ -19,23 +19,6 @@ function run_simulation(grid_params, rf_params, bioheat_params, boundary_conditi
     Qel, E_new = calculate_values(E, rf_params.sigma, grid_params)
     V_new = convert_V(V, rf_params.sigma, grid_params)
 
-
-
-    #=
-    # Initial temperature guess
-    T_old = zeros(Float64, ndofs(dh))
-
-    # Create timelapse or final visualization
-    if create_timelapse
-        TimelapseCreation.create_timelapse(grid, T_old, Qel, bioheat_params, grid_params)
-    else
-        # Solve bioheat equation without creating a timelapse
-        T = BioheatSolver.solve_bioheat(grid, T_old, Qel, bioheat_params, grid_params)
-
-        # Process and visualize the final temperature distribution
-        PlottingAndVisualization.plot_slices(V, Qel, grid_params, reshape(V, grid_params.nx, grid_params.ny), reshape(Qel, grid_params.nx, grid_params.ny), T)
-    end
-    =#
     return grid, V, E, Qel, E_new, V_new
 end
 
