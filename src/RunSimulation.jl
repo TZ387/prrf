5b2ddd5d521ff9ce30e358e4b5ed5783842b110a
Tilen Zel <tilen.zel@gmail.com>
2026-06-08 10:52:10 +0200

    Add few print statements to guide end user through simulation

--- src/RunSimulation.jl
+++ src/RunSimulation.jl
@@ -9,12 +9,14 @@ using ..PlottingAndVisualization
 export run_simulation
 
 function run_simulation(grid_params, rf_params, bioheat_params, boundary_conditions)
-
-    # Setup grid and solve RF problem
+    n_cells = grid_params.nx * grid_params.ny * grid_params.nz
+    @info "Setting up grid ($(grid_params.nx)×$(grid_params.ny)×$(grid_params.nz), $n_cells cells)..."
     grid = GridSetup.setup_grid(grid_params)
+
+    @info "Solving RF problem..."
     V_dof, dh, cellValues = solve_rf(grid, rf_params, grid_params, boundary_conditions)
-    
-    # Calculate E and Qel
+
+    @info "Computing electric field and thermal dissipation..."
     Qel, E = calculate_fields(cellValues, dh, V_dof, rf_params.sigma, grid_params)
     V = convert_V(V_dof, grid_params)
 
